22c0107655ebf6e30f70ee85c9a5acdb3a12a49a
kawa <kawa@gitlab.com>
2024-01-11 19:42:44 +0100

    add migration to dockerfile

--- app/portfolio/settings.py
+++ app/portfolio/settings.py
@@ -24,7 +24,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv('ENV') == 'development' else False
