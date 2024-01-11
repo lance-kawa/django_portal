14ffdb9776dd9d125711621267345288cefdda9a
kawa <kawa@gitlab.com>
2024-01-11 20:46:09 +0100

    try another approach wxith allowed_host

--- app/portfolio/settings.py
+++ app/portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv('ENV') == 'development' else False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(' ')
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost:1337').split(' ')
 
